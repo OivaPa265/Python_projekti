65f4109003ef711f9cd41c98ef8706d07e469003
Oiva <oiva.paatsema@gmail.com>
2026-02-27 12:44:54 +0200

    Refactor lore text and text wrapping settings
    Updated the lore to fit the new game parameters

--- Pohja/Lore.py
+++ Pohja/Lore.py
@@ -1,14 +1,15 @@
 import textwrap
-# fix yo shit
-lore =  (" Tervetuloa TXK26K1-B ohjelmisto 1  python peli projektiin tämän pelin ovat tehneet: nimi 1,2,3,4,? "
-         " Tässä pelissä pelaat humaloituneena joulupukkina hänen korkea koulu opiskelija aikana metropoliassa."
-         "Tehtäväsi on simppeli matkusta ympäri suomea keräämässä kaikenlaisia merkkejä joita voit kiinitää haalareihisi."
-         "Keräämäsi merkit näkyvät joulupukki hahmossasi joka näkyy nurkassa ."
-         " jotta voit liikua ympäri suomea tarvitset tietysti poltto ainetta, poltto aine on tietystikkin alkoholi.")
 
-wrapper = textwrap.TextWrapper(width=100, break_long_words=False, replace_whitespace=False)
+lore =  (" Tervetuloa TXK26K1-B ohjelmisto 1  python peli projektiin tämän pelin ovat tehneet: 1,2,3,4? "
+         " Tässä pelissä pelaat humaloituneena joulupukkina hänen korkeakoulu opiskelija aikanaan Metropoliassa."
+         "Tehtäväsi on simppeli matkusta ympäri suomea keräämässä kaikenlaisia merkkejä joita voit kiinittää haalareihisi."
+         "Keräämäsi merkit näkyvät joulupukki hahmossasi joka näkyy nurkassa ."
+         " jotta voisit liikua ympäri suomea tarvitset tietysti poltto ainetta, poltto aine on tietystikkin alkoholi."
+         "Onnea matkaan peelaja ja  varo kelaa, kela haluaa sinun alkoholisi. ole varovainen")
+
+wrapper = textwrap.TextWrapper(width=60, break_long_words=False, replace_whitespace=False)
 
 word_list = wrapper.wrap(text=lore)
 
-def Paheelore():
+def paheelore():
     return word_list
